50d61677000e524ec16752f3da54b21abc038d4e
GustavoCesarSantos <gustavocs789@gmail.com>
2023-03-20 16:27:52 -0300

    feat: cria repositorio para abstração do orm mikro

--- desafio1/src/autores/repositorio/criar-autor/criar-autor-mikro.repository/criar-autor-mikro.repository.ts
+++ desafio1/src/autores/repositorio/criar-autor/criar-autor-mikro.repository/criar-autor-mikro.repository.ts
@@ -1,1 +1,7 @@
-export class CriarAutorMikroRepository {}
+import { Autor } from 'src/autores/autor.entity';
+
+export class CriarAutorMikroRepository {
+  criar(autor: Autor): Promise<Autor> {
+    throw new Error('Method not implemented.');
+  }
+}
