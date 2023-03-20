25ff58723a5e906f0f02eb79cc3dcbe8fe310a30
GustavoCesarSantos <gustavocs789@gmail.com>
2023-03-20 02:02:06 -0300

    feat(Autores): cria repositório em memória para criar autor

---
+++ desafio1/src/autores/repositorio/criar-autor-memoria.repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+import { Autor } from '../autor.entity';
+
+@Injectable()
+export class CriarAutorEmMemoriaRepository {
+  private db: Autor[] = [];
+
+  public criar(autor: Autor): Autor {
+    this.db.push(autor);
+    return autor;
+  }
+}
