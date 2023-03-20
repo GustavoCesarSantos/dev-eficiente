64e22a45761b01bd942d9d9cd78b017ccf1ca34e
GustavoCesarSantos <gustavocs789@gmail.com>
2023-03-20 02:05:31 -0300

    refactor(Autores): refatora módulo de autores

--- desafio1/src/autores/autores.module.ts
+++ desafio1/src/autores/autores.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AutoresController } from './autores.controller';
+import { CriarAutorService } from './casos-de-uso/criar-autor/criar-autor.service';
+import { CriarAutorEmMemoriaRepository } from './repositorio/criar-autor-memoria.repository';
 
 @Module({
   imports: [],
   controllers: [AutoresController],
-  providers: [],
+  providers: [CriarAutorService, CriarAutorEmMemoriaRepository],
 })
 export class AutoresModule {}
