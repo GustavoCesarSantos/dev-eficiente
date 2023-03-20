734c3ae65871e4f94167db7bbadf29faf7b93256
GustavoCesarSantos <gustavocs789@gmail.com>
2023-03-20 02:05:01 -0300

    refactor(Autores): refatora controle de criação de autor

--- desafio1/src/autores/autores.controller.ts
+++ desafio1/src/autores/autores.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 
-import { CriarAutorDTO } from './autores.dto';
+import { Autor } from './autor.entity';
+import { CriarAutorService } from './casos-de-uso/criar-autor/criar-autor.service';
+import { CriarAutorDTO } from './dto/autores.dto';
 
 @Controller('autores')
 export class AutoresController {
+  constructor(private criarAutorService: CriarAutorService) {}
+
   @Post()
-  criarAutor(@Body() criarAutorDTO: CriarAutorDTO, @Res() response: Response) {
-    return response.status(200).json({ created: 'ok' });
+  public async criarAutor(
+    @Body() criarAutorDTO: CriarAutorDTO,
+    @Res() response: Response,
+  ): Promise<Response<Autor>> {
+    const autor = await this.criarAutorService.executar(criarAutorDTO);
+    return response.status(200).json(autor);
   }
 }
