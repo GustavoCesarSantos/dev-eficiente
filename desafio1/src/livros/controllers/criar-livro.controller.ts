04133b93590fd8b8e9625c8fdbb872f858da9c82
GustavoCesarSantos <gustavocs789@gmail.com>
2023-05-13 16:06:33 -0300

    refactor: refatora endpoint de criação de livro

--- desafio1/src/livros/controllers/criar-livro.controller.ts
+++ desafio1/src/livros/controllers/criar-livro.controller.ts
@@ -1,22 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Post } from '@nestjs/common';
 
 import { CriarLivroMemoriaRepository } from '../repositorios/criar-livro/criar-livro-memoria.repository/criar-livro-memoria.repository';
-import { CriarLivroRequest } from '../dto/livros.dto';
-import { Livro } from '../livro.entity';
+import { CriarLivroRequest, CriarLivroResponse } from '../dto/livros.dto';
+import { EncontrarAutorMemoriaRepository } from '../../autores/repositorio/encontrar-autor-memoria.repository';
+import { EncontrarCategoriaMemoriaRepository } from '../../categorias/repositorios/encontrar-categoria-memoria.repository';
 
 @Controller('livros')
 export class CriarLivroController {
   constructor(
+    private readonly encontrarAutorMemoriaRepository: EncontrarAutorMemoriaRepository,
+    private readonly encontrarCategoriaMemoriaRepository: EncontrarCategoriaMemoriaRepository,
     private readonly criarLivroMemoriaRepository: CriarLivroMemoriaRepository,
   ) {}
 
   @Post()
   public async criarLivo(
     @Body() criarLivroDTO: CriarLivroRequest,
-  ): Promise<Livro> {
+  ): Promise<CriarLivroResponse> {
     try {
       const novoLivro = criarLivroDTO.toModel();
-      return this.criarLivroMemoriaRepository.criar(novoLivro);
+      const autor = await this.encontrarAutorMemoriaRepository.encontrar(
+        novoLivro.getIdAutor(),
+      );
+      if (!autor) throw new NotFoundException();
+      const categoria =
+        await this.encontrarCategoriaMemoriaRepository.encontrar(
+          novoLivro.getIdCategoria(),
+        );
+      if (!categoria) throw new NotFoundException();
+      const livro = this.criarLivroMemoriaRepository.criar(novoLivro);
+      const criarLivroResponse = new CriarLivroResponse(
+        livro,
+        autor,
+        categoria,
+      );
+      return criarLivroResponse;
     } catch (error) {
       return error;
     }
