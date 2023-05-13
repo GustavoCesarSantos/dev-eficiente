31100b199b9e104cdab06e07eeaa5357ea776756
GustavoCesarSantos <gustavocs789@gmail.com>
2023-05-13 16:04:25 -0300

    feat: cria dto de resposta para a criação do livro

--- desafio1/src/livros/dto/livros.dto.ts
+++ desafio1/src/livros/dto/livros.dto.ts
@@ -11,6 +11,8 @@ import {
 import { IsUnique } from '../../customValidations/is-unique';
 import { IsFuture } from '../../customValidations/is-future';
 import { Livro } from '../livro.entity';
+import { Categoria } from '../../categorias/categoria.entity';
+import { Autor } from '../../autores/autor.entity';
 
 export class CriarLivroRequest {
   @IsString()
@@ -54,6 +56,63 @@ export class CriarLivroRequest {
 
   public toModel(): Livro {
     return new Livro({ ...this });
+  }
+}
+
+export class CriarLivroResponse {
+  @IsString()
+  @IsNotEmpty()
+  titulo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 500)
+  resumo: string;
+
+  @IsString()
+  sumario: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(20)
+  preco: number;
+
+  @IsInt()
+  @Min(100)
+  numeroPaginas: number;
+
+  @IsString()
+  @IsNotEmpty()
+  isbn: string;
+
+  @IsDateString()
+  @IsFuture()
+  dataPublicacao: string;
+
+  @IsString()
+  @IsNotEmpty()
+  categoria: string;
+
+  autor: {
+    nome: string;
+    email: string;
+    descricao: string;
+  };
+
+  constructor(livro: Livro, autor: Autor, categoria: Categoria) {
+    this.titulo = livro.getTitulo();
+    this.resumo = livro.getResumo();
+    this.sumario = livro.getSumario();
+    this.preco = livro.getPreco();
+    this.numeroPaginas = livro.getNumeroPaginas();
+    this.isbn = livro.getIsbn();
+    this.dataPublicacao = livro.getDataPublicacao();
+    this.categoria = categoria.getNome();
+    this.autor = {
+      nome: autor.getNome(),
+      email: autor.getEmail(),
+      descricao: autor.getDescricao(),
+    };
   }
 }
 
