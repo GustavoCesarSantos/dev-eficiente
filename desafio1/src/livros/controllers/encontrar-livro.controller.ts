72de457d3358bb6526a52ee382efa3cb84fd6de2
GustavoCesarSantos <gustavocs789@gmail.com>
2023-05-21 11:40:26 -0300

    refactor: refatora endpoint encontrar livro

--- desafio1/src/livros/controllers/encontrar-livro.controller.ts
+++ desafio1/src/livros/controllers/encontrar-livro.controller.ts
@@ -1,23 +1,43 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 
 import { EncontrarLivroMemoriaRepository } from '../repositorios/encontrar-livro/encontrar-livro-memoria.repository/encontrar-livro-memoria.repository';
-import { EncontrarLivroRequest } from '../dto/livros.dto';
-import { Livro } from '../livro.entity';
+import {
+  EncontrarLivroRequest,
+  EncontrarLivroResponse,
+} from '../dto/livros.dto';
+import { EncontrarAutorMemoriaRepository } from '../../autores/repositorio/encontrar-autor-memoria.repository';
+import { EncontrarCategoriaMemoriaRepository } from '../../categorias/repositorios/encontrar-categoria-memoria.repository';
 
 @Controller('livros')
 export class EncontrarLivroController {
   constructor(
     private readonly encontrarLivroMemoriaRepository: EncontrarLivroMemoriaRepository,
+    private readonly encontrarAutorMemoriaRepository: EncontrarAutorMemoriaRepository,
+    private readonly encontrarCategoriaMemoriaRepository: EncontrarCategoriaMemoriaRepository,
   ) {}
 
   @Get(':id')
   public async encontrarLivro(
     @Param() params: EncontrarLivroRequest,
-  ): Promise<Livro> {
+  ): Promise<EncontrarLivroResponse> {
     try {
       const livro = this.encontrarLivroMemoriaRepository.encontrar(params.id);
       if (!livro) throw new NotFoundException('Livro não encontrado.');
-      return livro;
+      const autor = await this.encontrarAutorMemoriaRepository.encontrar(
+        livro.getIdAutor(),
+      );
+      if (!autor) throw new NotFoundException('Autor não encontrado.');
+      const categoria =
+        await this.encontrarCategoriaMemoriaRepository.encontrar(
+          livro.getIdCategoria(),
+        );
+      if (!categoria) throw new NotFoundException('Categoria não encontrada.');
+      const encontrarLivroResponse = new EncontrarLivroResponse(
+        livro,
+        autor,
+        categoria,
+      );
+      return encontrarLivroResponse;
     } catch (error) {
       throw error;
     }
--- desafio1/src/livros/dto/livros.dto.ts
+++ desafio1/src/livros/dto/livros.dto.ts
@@ -135,3 +135,60 @@ export class EncontrarLivroRequest {
   @IsUUID()
   id: string;
 }
+
+export class EncontrarLivroResponse {
+  @IsString()
+  @IsNotEmpty()
+  titulo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 500)
+  resumo: string;
+
+  @IsString()
+  sumario: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(20)
+  preco: number;
+
+  @IsInt()
+  @Min(100)
+  numeroPaginas: number;
+
+  @IsString()
+  @IsNotEmpty()
+  isbn: string;
+
+  @IsDateString()
+  @IsFuture()
+  dataPublicacao: string;
+
+  @IsString()
+  @IsNotEmpty()
+  categoria: string;
+
+  autor: {
+    nome: string;
+    email: string;
+    descricao: string;
+  };
+
+  constructor(livro: Livro, autor: Autor, categoria: Categoria) {
+    this.titulo = livro.getTitulo();
+    this.resumo = livro.getResumo();
+    this.sumario = livro.getSumario();
+    this.preco = livro.getPreco();
+    this.numeroPaginas = livro.getNumeroPaginas();
+    this.isbn = livro.getIsbn();
+    this.dataPublicacao = livro.getDataPublicacao();
+    this.categoria = categoria.getNome();
+    this.autor = {
+      nome: autor.getNome(),
+      email: autor.getEmail(),
+      descricao: autor.getDescricao(),
+    };
+  }
+}
